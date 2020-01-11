eb5743c278714ba7e0930491a8ea80cf1ea08829
Allen <allencch@hotmail.com>
2020-01-11 15:37:32 +0800

    Initial commit

---
+++ proton_wrapper.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+_PROTON_VERSION=4.11
+CALL_WINETRICKS=false
+CALL_CONSOLE=false
+CALL_SHUTDOWN=false
+CALL_REBOOT=false
+CALL_KILL=false
+CALL_WINEBOOT=false
+CALL_WINE=false
+ARGS=()
+
+echo "$#"
+if [ "$#" -le "1" ] ; then
+    echo -e "Usage: proton_wrapper.sh -p <wineprefix> -v <version> arguments ..."
+    exit
+fi
+
+while [ "$1" != "" ]; do
+    case $1 in
+        -p | --prefix)
+            shift
+            _WINEPREFIX="$1"
+            ;;
+        --shutdown)
+            CALL_SHUTDOWN=true
+            ;;
+        --reboot)
+            CALL_REBOOT=true
+            ;;
+        --kill)
+            CALL_KILL=true
+            ;;
+        --wineboot)
+            CALL_WINEBOOT=true
+            ;;
+        --version)
+            _PROTON_VERSION="$1"
+            ;;
+        --winetricks)
+            CALL_WINETRICKS=true
+            ;;
+        --wine)
+            CALL_WINE=true
+            ;;
+        *)
+            ARGS+=("$1")
+    esac
+    shift
+done
+
+if [ ! -d "$_WINEPREFIX" ]; then
+    mkdir -p "$_WINEPREFIX"
+fi
+export STEAM_COMPAT_DATA_PATH="$_WINEPREFIX"
+export _PROTON_DIR="$HOME/.steam/steam/steamapps/common/Proton $_PROTON_VERSION"
+export _PROTON="$_PROTON_DIR/proton"
+
+function setup_env() {
+    export WINEPREFIX="$_WINEPREFIX/pfx"
+    export PATH="$_PROTON_DIR/dist/bin:$PATH"
+    export LD_LIBRARY_PATH="$_PROTON_DIR/dist/lib:$_PROTON_DIR/dist/lib64:$LD_LIBRARY_PATH"
+    export WINEDLLPATH="$_PROTON_DIR/dist/lib/wine/fakedlls:$_PROTON_DIR/dist/lib64/wine/fakedlls"
+    export WINEDEBUG=fixme-all
+}
+
+
+if [ "$CALL_WINETRICKS" = true ]; then
+    setup_env
+    winetricks "${ARGS}"
+elif [ "$CALL_WINE" = true ]; then
+    setup_env
+    "$_PROTON_DIR/dist/bin/wine" "${ARGS}"
+elif [ "$CALL_SHUTDOWN" = true ]; then
+    "$_PROTON" run wineboot -s
+elif [ "$CALL_REBOOT" = true ]; then
+    "$_PROTON" run wineboot -r
+elif [ "$CALL_KILL" = true ]; then
+    "$_PROTON" run wineserver -k
+elif [ "$CALL_WINEBOOT" = true ]; then
+    "$_PROTON" run wineboot "${ARGS}"
+else
+    "$_PROTON" run "${ARGS}"
+fi
---
+++ wine_wrapper.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+_WINEARCH=win64
+_WINEPREFIX="$HOME/.wine"
+_WINEDLLOVERRIDES="winemenubuilder.exe=d;mscoree;mshtml"
+_WINEDEBUG=fixme-all
+_WINEPATH=""
+_WINEBIN=wine
+CALL_WINETRICKS=false
+CALL_CONSOLE=false
+CALL_SHUTDOWN=false
+CALL_REBOOT=false
+CALL_KILL=false
+CALL_WINEBOOT=false
+ARGS=()
+
+if [ "$#" -le "1" ] ; then
+    echo -e "Usage: wine_wrapper.sh -p <wineprefix> arguments ..."
+    exit
+fi
+
+while [ "$1" != "" ]; do
+    case $1 in
+        -p | --prefix)
+            shift
+            _WINEPREFIX="$1"
+            ;;
+        --winetricks)
+            CALL_WINETRICKS=true
+            ;;
+        --win32)
+            _WINEARCH=win32
+            ;;
+        --console)
+            CALL_CONSOLE=true
+            ;;
+        --shutdown)
+            CALL_SHUTDOWN=true
+            ;;
+        --reboot)
+            CALL_REBOOT=true
+            ;;
+        --kill)
+            CALL_KILL=true
+            ;;
+        --winepath)
+            shift
+            _WINEPATH="$1"
+            ;;
+        --wine)
+            shift
+            _WINEBIN="$1"
+            ;;
+        --wineboot)
+            CALL_WINEBOOT=true
+            ;;
+        *)
+            ARGS+=("$1")
+    esac
+    shift
+done
+
+export WINEARCH="$_WINEARCH"
+export WINEDLLOVERRIDES="$_WINEDLLOVERRIDES"
+export WINEPREFIX="$_WINEPREFIX"
+export WINEDEBUG="$_WINEDEBUG"
+
+if [ -n "$_WINEPATH" ]; then
+    export PATH="$_WINEPATH/bin:$PATH"
+
+    # https://gist.github.com/shmerl/a2867c5a675ed1795f03326b32b47fe7
+    # May need to add arch suffix
+    export LD_LIBRARY_PATH="$_WINEPATH/lib:$_WINEPATH/lib64:$_WINEPATH/lib32:$LD_LIBRARY_PATH"
+fi
+
+if [ "$CALL_WINETRICKS" = true ]; then
+    winetricks "${ARGS}"
+elif [ "$CALL_CONSOLE" = true ]; then
+    wineconsole
+elif [ "$CALL_SHUTDOWN" = true ]; then
+    wineboot -s
+elif [ "$CALL_REBOOT" = true ]; then
+    wineboot -r
+elif [ "$CALL_KILL" = true ]; then
+    wineserver -k
+elif [ "$CALL_WINEBOOT" = true ]; then
+    wineboot "${ARGS}"
+else
+    $_WINEBIN "${ARGS}"
+fi
