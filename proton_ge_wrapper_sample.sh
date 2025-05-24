cb3fee6038c74fc9ae96948d03c33c84a53db378
Allen <allencch@hotmail.com>
2025-05-24 23:46:29 +0800

    chore: sample proton_ge_wrapper

---
+++ proton_ge_wrapper_sample.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Proton GE7
+
+#export _BASE_PATH="$HOME/mywines/wines/GE-Proton7-51"
+#export _BASE_PATH="$HOME/mywines/wines/GE-Proton8-11"
+export _BASE_PATH="$HOME/mywines/wines/GE-Proton9-11"
+sh "$HOME/path/to/proton_ge_base_wrapper.sh" "$@"
